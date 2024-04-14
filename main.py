88a0883e90a47e260af997fb231bc9d34faa581d
Refael <refa3211@gmail.com>
2024-04-15 01:23:20 +0300

    update repo

--- main.py
+++ main.py
@@ -1,35 +1,57 @@
 # import requests
 import os
+from platform import python_branch
+import re
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+
+import pyotp
 import time
 load_dotenv() 
 
 
 username = os.getenv('USER')
 password = os.getenv('PASS')
-print(username, password)
+OTPCODE = os.getenv('OTPSECERT')
+
+
+def otpcode():
+    totp = pyotp.TOTP(OTPCODE)
+    print(totp.now())
+    return totp.now()
+
 
 
 def login():
     
     driver = webdriver.Chrome()  # Assuming you have Chrome WebDriver installed
     driver.get("https://vpn5.health.gov.il")
-    time.sleep(3)
+    time.sleep(2)
 
+    #username
     email_input = driver.find_element(By.ID, "input_1")
     email_input.send_keys(username)
-    time.sleep(3)
+    time.sleep(1.5)
+    #password
     password_input = driver.find_element(By.ID, "input_2")
     password_input.send_keys(password)
-    time.sleep(1)
+    time.sleep(1.5)
+    #select auth method 
     dropdown = Select(driver.find_element(By.ID, "input_3"))  # Replace "dropdown_id" with the actual ID of the dropdown
     dropdown.select_by_visible_text("Google_Authenticator")
-    # password_input.send_keys(Keys.RETURN)
+    password_input.send_keys(Keys.RETURN)
+    time.sleep(1.5)
 
-    time.sleep(5) 
+    #page 2 enter otp code
+    otpinput = driver.find_element(By.ID, "input_1")
+    otpinput.send_keys(otpcode())
+    time.sleep(1)
+    password_input.send_keys(Keys.RETURN)
+
+    time.sleep(20) 
     
   
     # driver.quit()
@@ -37,3 +59,4 @@ def login():
 
 if __name__ == "__main__":
     login()
+    otpcode()
